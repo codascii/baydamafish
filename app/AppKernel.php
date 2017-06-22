510d3cbdfb105c42e26e24c6450f962b5ebc3cb5
codascii <houmadi.bacar.mohamed@gmail.com>
2017-06-23 00:33:16 +0200

    Ajout de 'Baydamafish/AppBundle' dans le projet

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Baydamafish\ProductBundle\BaydamafishProductBundle(),
+            new Baydamafish\AppBundle\BaydamafishAppBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
